a8ac05c3a4e17467a4120a7a48c6c581912a8f9f
sushantk-mhty <sushant.mhty@outlook.com>
2022-12-22 18:09:41 +0530

    SeedVilla Table with Records

---
+++ MagicVilla_VillaAPI/Migrations/20221222122454_SeedVillaTableWithCreatedDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTableWithCreatedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
