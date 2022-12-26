64d534dd9af866a0ed724b00bc758e15238e34f6
sushantk-mhty <sushant.mhty@outlook.com>
2022-12-26 12:06:33 +0530

    API Response

---
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Models
+{
+	public class APIResponse
+	{
+		public APIResponse()
+		{
+		}
+		public HttpStatusCode StatusCode { get; set; }
+		public bool IsSuccess { get; set; }
+		public List<string> ErrorMessages { get; set; }
+		public object Result { get; set; }
+	}
+}
+
