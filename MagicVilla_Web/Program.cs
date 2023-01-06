9cccc7cda27740fe8896d70c7b2919950707bd58
sushantk-mhty <sushant.mhty@outlook.com>
2023-01-06 16:51:57 +0530

    Adding Villa Service

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,4 +1,6 @@
 ﻿using MagicVilla_Web;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 
+
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
 
 var app = builder.Build();
 
