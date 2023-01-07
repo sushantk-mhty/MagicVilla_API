cb654e714bb81f4c63c746c758767ee892d8e8ca
sushantk-mhty <sushant.mhty@outlook.com>
2023-01-07 12:36:19 +0530

    Display all villa list

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 
